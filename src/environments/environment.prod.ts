26d391153333e4ad32b83ae1cacf058c9c7b4126
Sudhakar Camilos <sudhakar.camilos@outlook.com>
2019-08-04 12:39:23 +0400

    some Ui changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyCfIHgznlzL-GZXT4OjUoXW6aiLQWQkLKA",
+    authDomain: "aipod-93063.firebaseapp.com",
+    databaseURL: "https://aipod-93063.firebaseio.com",
+    projectId: "aipod-93063",
+    storageBucket: "aipod-93063.appspot.com",
+    messagingSenderId: "139073064610",
+    appId: "1:139073064610:web:838a55d09d77d65d"
+  }
 };
